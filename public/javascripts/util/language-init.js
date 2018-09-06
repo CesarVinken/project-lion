c8890902b64059402d706d33468ae575c87d6ebf
Cesar Vinken <cesarvinken@hotmail.com>
2018-09-06 11:28:52 +0200

    language now defined by name, not language code

--- public/javascripts/util/language-init.js
+++ public/javascripts/util/language-init.js
@@ -2,7 +2,7 @@ function getLanguageList() {
   let languageList = `<option value="">Select a language</option>`;
 
   for (const language in isoLangs) {
-    languageList += `<option value='${language}'>${
+    languageList += `<option value='${isoLangs[language].name}'>${
       isoLangs[language].name
     }</option>`;
   }
@@ -13,32 +13,38 @@ function getLanguageList() {
 function getEventLanguageList() {
   let languageList = "";
 
-  let firstLanguage = "en"; //if the user's known languages is empty, fall back to English
+  let firstLanguage = "English"; //if the user's known languages is empty, fall back to English
   if (data.user.knownLanguages.length > 0)
     firstLanguage = data.user.knownLanguages[0];
 
   for (const language in isoLangs) {
     if (language === firstLanguage) {
-      languageList += `<option class="item" data-value='${language}' selected="selected">${
+      languageList += `<option class="item" data-value='${
         isoLangs[language].name
-      }</option>`;
+      }' selected="selected">${isoLangs[language].name}</option>`;
     } else {
-      languageList += `<option class="item" data-value='${language}'>${
+      languageList += `<option class="item" data-value='${
         isoLangs[language].name
-      }</option>`;
+      }'>${isoLangs[language].name}</option>`;
     }
   }
   $("#event-language").html(languageList);
 }
 
 function setUserLanguages() {
+  console.log("set LANGUAGE");
+
   if (user.knownLanguages) {
     user.knownLanguages.forEach(language => {
+      console.log("set selected language: " + language);
+
       $("#multi-select-known").dropdown("set selected", language);
     });
   }
   if (user.learningLanguages) {
     user.learningLanguages.forEach(language => {
+      console.log("set selected language: " + language);
+
       $("#multi-select-learning").dropdown("set selected", language);
     });
   }
