a9af0ea7cfd59261ad3854e5213e76b887a28da9
Cesar Vinken <cesarvinken@hotmail.com>
2018-09-08 23:51:51 +0200

    Added localisation system. Implemented l18n keys for nav bar and Dutch/English translations

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -13,4 +13,48 @@ $(document).ready(function() {
       }
     }
   });
+
+  i18next.on("missingKey", function(lngs, namespace, key, res) {
+    console.log(
+      `Key ${key} missing for the language '${
+        i18next.language
+      }'. Please add a translation.`
+    );
+  });
 });
+
+i18next.init(
+  {
+    lng: "en",
+    resources: {
+      en: {},
+      de: {}
+    }
+  },
+  function(err, t) {
+    jqueryI18next.init(i18next, $);
+
+    const language = i18next.language;
+
+    axios.get(`/l18n/${language}.json`).then(res => {
+      localiseTexts(language, res);
+    });
+  }
+);
+
+function SetLanguage(language) {
+  console.log("set language to " + language);
+
+  i18next.changeLanguage(language, (err, t) => {
+    if (err) return console.log("something went wrong loading", err);
+    axios.get(`/l18n/${language}.json`).then(res => {
+      localiseTexts(language, res);
+    });
+  });
+}
+
+function localiseTexts(language, strings) {
+  i18next.addResourceBundle(language, "translation", strings.data, true, true);
+
+  $("*").localize(); //it is also possible to load parts of the translation, by setting the right tag.
+}
